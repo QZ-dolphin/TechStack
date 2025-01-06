1a7d2030b6a932f8f6a5c9bdbd5102794ef3b153
qingzhen_ubuntu <troyartum@outlook.com>
2025-01-06 20:24:03 +0800

    revise_2025-01-06_20-24-03

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,72 @@
   <div class="title">
     Personal Tech Stack
   </div>
-  <div style="color: gray; margin-left: 25px;">
+  <div style="color: gray; margin-left: 25px; height: 2vh;">
     &#9824; Recent Update Time: {{ updateTime }}
   </div>
   <div>
-    <component :is="component" v-model="showCtx"></component>
+    <component :is="component" v-model:show="showCtx" v-model:content="userdata"></component>
   </div>
   <div class="center">
-    <el-button v-if="!showCtx" @click="showCtx = !showCtx" type="primary" :icon="Edit" circle />
-    <el-button v-if="showCtx" @click="showCtx = !showCtx" type="success" :icon="Check" circle />
+    <el-button v-if="!showCtx" @click="editdata" type="primary" :icon="Edit" circle />
+    <el-button v-if="showCtx" @click="savedata" type="success" :icon="Check" circle />
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import VueComposition from './components/vue-composition.vue';
-import { Edit, Check } from '@element-plus/icons-vue'
+import axios from 'axios';
+import { Edit, Check } from '@element-plus/icons-vue';
 
-// const GO_API_URL = process.env.VUE_APP_GO_API_URL
+const GO_API_URL = import.meta.env.VITE_GO_API_URL;
+const updateTime = ref('2025/01/05 23:42');
+const userdata = ref('# Tech Stack');
+var tempuserdata = '';
 const type = ref('composition');
-const updateTime = '2025/01/05 23:42';
+
 const component = computed(() => ({
   composition: VueComposition,
 }[type.value]));
 
 const showCtx = ref(false);
-// console.log(GO_API_URL)
-console.log(import.meta.env.VITE_GO_API_URL);
+
+async function fetchdate() {
+  let params = '?username=' + 'dongqingzhen';
+  const response = await axios.get(GO_API_URL + '/userdata' + params);
+  console.log(response.data);
+  updateTime.value = response.data.updated_at;
+  userdata.value = response.data.stackdata;
+  console.log(userdata.value);
+}
+
+onMounted( async () => {
+  await fetchdate();
+});
+
+function editdata(){
+  showCtx.value = !showCtx.value;
+  tempuserdata = userdata.value;
+}
+
+async function savedata(){
+  showCtx.value = !showCtx.value;
+  if(tempuserdata==userdata.value){
+    return;
+  }
+  let data = {
+    username: 'dongqingzhen',
+    userdata: userdata.value,
+  };
+  try {
+    const response = await axios.post(`${GO_API_URL}/userupdate`, data);
+    console.log('Response from POST request:', response.data);
+    // 你可以在这里处理响应数据
+  } catch (error) {
+    console.error('Error posting data:', error);
+  }
+  await fetchdate();
+}
 </script>
 
 <style scoped>
--- src/components/vue-composition.vue
+++ src/components/vue-composition.vue
@@ -1,77 +1,69 @@
 <template>
   <div class="center-container">
     <svg class="pics" ref="svgRef"></svg>
-    <MarkdownEditor class="text" v-if="showCtx" v-model="content" />
+    <MarkdownEditor class="text" v-if="showCtx" v-model="userdata" />
   </div>
+
 </template>
 
-<script setup lang="ts">
-  import { ref, onMounted, onUpdated } from 'vue';
-  import { Markmap } from 'markmap-view';
-  import { transformer } from './markmap.js';
-  import MarkdownEditor from './MarkdownEditor.vue';
+<script setup>
+import { ref, onMounted, onUpdated } from 'vue';
+import { Markmap } from 'markmap-view';
+import { transformer } from './markmap.js';
+import MarkdownEditor from './MarkdownEditor.vue';
 
-  const showCtx = defineModel()
-  const svgRef = ref();
-  const content = ref('');
-  let mm;
+const userdata = defineModel('content');
+const showCtx = defineModel('show')
 
-  const update = () => {
-    // console.log(content.value);
-    const { root } = transformer.transform(content.value);
+const svgRef = ref();
+let mm;
+const update = () => {
+  const { root } = transformer.transform(userdata.value);
+  if (mm) {
     mm.setData(root);
     mm.fit();
-  };
+  }
+};
 
-  onMounted(async () => {
-    try {
-      const response = await fetch('/1.txt');
-      if (response.ok) {
-        content.value = await response.text();
-        // console.log(content.value)
-      } else {
-        console.error('Failed to load file:', response.statusText);
-      }
-    } catch (error) {
-      console.error('Error fetching file:', error);
-    }
-    mm = Markmap.create(svgRef.value);
-    update();
-  });
+onMounted(async () => {
+  mm = Markmap.create(svgRef.value);
+  update();
+});
 
-  onUpdated(update);
+onUpdated(update);
 
 
 </script>
 
 <style scoped>
-  .center-container {
-    display: flex;
-    /* 使用 Flexbox 布局 */
-    justify-content: space-between;
-    /* 使子元素在主轴上两端对齐 */
-    width: 100%;
-    /* 确保容器宽度占满父元素 */
-    align-items: center;
-    /* 使子元素在交叉轴上居中对齐 */
-    height: 80vh;
-    /* 如果需要垂直居中，确保容器有高度 */
-    width: 100%;
-  }
+.center-container {
+  display: flex;
+  /* 使用 Flexbox 布局 */
+  justify-content: space-between;
+  /* 使子元素在主轴上两端对齐 */
+  width: 100%;
+  /* 确保容器宽度占满父元素 */
+  align-items: center;
+  /* 使子元素在交叉轴上居中对齐 */
+  height: 80vh;
+  /* 如果需要垂直居中，确保容器有高度 */
+  width: 100%;
+}
 
-  .pics {
-    flex: 1;
-    /* 使子元素等分剩余空间 */
-    margin: 0 10px;
-    /* 添加一些间距 */
-    height: 100%
-  }
 
-  .text {
-    flex: 1;
-    /* 使子元素等分剩余空间 */
-    margin: 0 10px;
-    /* 添加一些间距 */
-    height: 80%;
-  }
+.pics {
+  flex: 1;
+  /* 使子元素等分剩余空间 */
+  margin: 0 10px;
+  /* 添加一些间距 */
+  height: 100%
+}
+
+.text {
+  flex: 1;
+  /* 使子元素等分剩余空间 */
+  margin: 0 10px;
+  /* 添加一些间距 */
+  height: 80%;
+}
 </style>
